d4413536e9e224d33ad8ec0dfcd1e97b9c536d9f
jackson <jacksonxu45252@gmail.com>
2018-05-26 11:49:24 +0800

    Init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # mopcon-website
 純前端的開發環境
+
+## 環境準備
+要使用 Gulp，第一步就是要安裝 Node.js [下載網址](http://nodejs.org/download/)
+安裝 node.js 也會順便把 npm 安裝進去，要注意 npm 版本過舊可能會導致之後 build 失敗  
+[更新 node, npm 參考資料](https://nodejs.org/en/download/package-manager/)
+
+安裝套件管理器至全域(在 Mac 要記得加 sudo 不然會裝不進去)
+```
+npm install -g gulp
+```
+
+安裝 build 網頁時會用到的前處理器
+```
+npm install
+```
+## 執行
+
+```
+gulp
+```
---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+var gulp        =  require('gulp'),
+    webserver   =  require('gulp-webserver'),
+    concat      =  require('gulp-concat'),
+    plumber     =  require('gulp-plumber'),
+    sass        =  require('gulp-sass'),
+    watch       =  require('gulp-watch'),
+    layout      =  require('gulp-layout'),
+    ejs         =  require('gulp-ejs'),
+    frontMatter = require('gulp-front-matter'),
+    // post css
+    postcss =     require('gulp-postcss'),
+    autoprefixer = require('autoprefixer');
+var config =      require('./gulpconfig');
+
+// Sass
+gulp.task('sass', function() {
+   var processors = [
+    autoprefixer(config.postcss.autoprefixer),
+  ];
+  gulp.src([config.paths.sass + '**/**.scss'])
+    .pipe(plumber())
+    .pipe(sass({outputStyle: config.sass.output_style})
+    .on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest(config.paths.public + config.paths.sass_output));
+});
+watch([config.paths.sass + '**/*.scss'], function() {
+  gulp.start('sass');
+});
+
+
+// 其它不編譯的物件
+var objs = [config.paths.source + '**/**.*'];
+for (var i = 0; i < config.others.length; i++) {
+  objs.push('!' + config.paths.source + config.others[i]);
+}
+gulp.task('others', function(){
+  return gulp.src(objs)
+    .pipe(plumber())
+    .pipe(gulp.dest(config.paths.public));
+});
+watch(objs, function() {
+  gulp.start('others');
+});
+
+// 樣版
+gulp.task('gulp-layout', function() {
+  return gulp.src([config.paths.source + '**/*.ejs',config.paths.source + '**/*.html','!' + config.paths.source + '_*/*.ejs','!' + config.paths.source + '/_*/*.html'])
+    .pipe(plumber())
+    .pipe(frontMatter({
+            property: 'data',
+            remove: true
+        }))
+    .pipe(ejs())
+    .pipe(layout(function(file) {
+      return file.data;
+}))
+
+    .pipe(gulp.dest(config.paths.public));
+});
+watch([config.paths.source + '**/*.ejs', config.paths.source + '**/*.html'], function(){
+  gulp.start('gulp-layout');
+});
+
+// webserver
+gulp.task('webserver', function() {
+  setTimeout(function(){
+    gulp.src(config.paths.public)
+      .pipe(webserver({
+        livereload: true,
+        open: true,
+        host: '0.0.0.0',
+        port: 4567
+        // fallback: '/Home/index.html'
+      }));
+  });
+});
+
+gulp.task('default', ['gulp-layout','others','sass','webserver']);
