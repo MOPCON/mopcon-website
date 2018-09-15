dc2ef74d7dc1a3aa087712c5e7e0b685cab21665
Hsien Wei <hsien.w.wei@gmail.com>
2018-09-15 13:45:49 +0800

    update scroll setting

---
+++ source/assets/js/warmUpScrollify.js
@@ -0,0 +1,49 @@
+$(function () {
+    $.scrollify({
+        section: ".panel",
+        // sectionName: false,
+        scrollSpeed: 1100,
+        overflowScroll: true,
+    });
+
+    $(window).scroll(function () {
+        const scrollVal = $(this).scrollTop();
+        if (scrollVal > 300){
+            $("#back-to-top").addClass('show-top-arrow');
+        } else {
+            $("#back-to-top").removeClass('show-top-arrow');
+        }
+    });
+
+    $.scrollify.disable();
+
+    $("#warm-up-link").click(function(){
+        warmupScrollify("#2");
+    });
+
+    $("#timetable-link").click(function(){
+        warmupScrollify("#3");
+    });
+
+    $("#ticket-link").click(function(){
+        warmupScrollify("#4");
+    });
+
+    $("#spthank-link").click(function(){
+        warmupScrollify("#5");
+    });
+
+    $("#warm-arrow-go").click(function(){
+        warmupScrollify("#3");
+    });
+
+    $("#back-to-top").click(function(){
+        warmupScrollify("#1");
+    });
+
+    function warmupScrollify(target) {
+        $.scrollify.enable();
+        $.scrollify.move(target);
+        $.scrollify.disable();
+    }
+});
